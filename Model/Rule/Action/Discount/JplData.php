e8ecb8ff647f144f8b5fbd3c381fd77ea6826496
root <root@localhost.localdomain>
2020-03-03 12:17:25 +0000

    Adding JplData to keep the total qty

---
+++ Model/Rule/Action/Discount/JplData.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace WCS\jplPromotions\Model\Rule\Action\Discount;
+
+/**
+ *  Jpl Discount Data
+ *
+ */
+class JplData {
+    /**
+     * @var float
+     */
+    protected $totalQty;
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->settotalQty(0);
+    }
+
+    /**
+     * Set total Qty
+     *
+     * @param float $totalQty
+     * @return $this
+     */
+    public function settotalQty($totalQty)
+    {
+        $this->totalQty = $totalQty;
+        return $this;
+    }
+
+    /**
+     * Get total Qty
+     *
+     * @return float
+     */
+    public function gettotalQty()
+    {
+        return $this->totalQty;
+    }
+
+}
