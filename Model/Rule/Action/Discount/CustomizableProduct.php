8e5ae69e7d732ca485421b767aff3b5a9acc5ec9
Sergio Abad <saaideveloper@gmail.com>
2020-03-08 15:30:00 +0000

    Removing discount for Rows with more that 20 items

--- Model/Rule/Action/Discount/CustomizableProduct.php
+++ Model/Rule/Action/Discount/CustomizableProduct.php
@@ -241,7 +241,7 @@ class CustomizableProduct extends AbstractDiscount
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 //If the individual row has more than 21
 if($jplData->gettotalQty() > 20){
-    $discountData->setAmount(3.50);
+    //$discountData->setAmount(3.50);
 }else{
     $totalInCart = 0;
     $regex='/\b'.$optValue.'\b/';
