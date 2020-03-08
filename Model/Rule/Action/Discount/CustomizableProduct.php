5d24d53b1acd82155e0066f5644feed749f483bd
Sergio Abad <saaideveloper@gmail.com>
2020-03-08 20:48:44 +0000

    Removing comment code

--- Model/Rule/Action/Discount/CustomizableProduct.php
+++ Model/Rule/Action/Discount/CustomizableProduct.php
@@ -190,43 +190,21 @@ class CustomizableProduct extends AbstractDiscount
 
         }
         
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-//BEGIN TO MOVE TO JPLData to a function applyDiscount()
-//variables $quote , $sku , $item , $x
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-//If the individual row has more than 21
-if($jplData->gettotalQty() < 0){
-    //$discountData->setAmount(3.50);
-}else{
-    $totalInCart = 0;
-    $regex='/\b'.$optValue.'\b/';
-    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    //BEGIN TO MOVE TO A FUNCTION IN HELPER totalInCartByProduct($quote->getAllVisibleItems)
-    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    /*
-    foreach($quote->getAllVisibleItems() as $_item) {
-        //echo 'Sku: '.$_item->getSku().'<br/>';
-        //echo 'Quantity: '.$_item->getQty().'<br/>';
-        //if ($_item->getSku() == $sku ){
-        if(preg_match($regex, $_item->getSku() )){
-            $totalInCart = $totalInCart + $_item->getQty();
+        //TOTAL QTY PER ROW IN CART
+        if($jplData->gettotalQty() < 0){
+            //$discountData->setAmount(3.50);
+        }else{
+            $totalInCart = 0;
+            $regex='/\b'.$optValue.'\b/';
+            //If($item->getProduct()->getSku() == $sku){
+            if(preg_match($regex, $item->getProduct()->getSku() )){
+                //if($totalInCart > $x){
+                if($this->getTotalInCart($quote,$regex) > $x){
+                    $discountData->setAmount($jplData->gettotalQty());
+                }
+            }
         }
-    }*/
-    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    //END TO MOVE TO A FUNCTION IN HELPER totalInCartByProduct($quote->getAllVisibleItems)
-    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    //If($item->getProduct()->getSku() == $sku){
-    if(preg_match($regex, $item->getProduct()->getSku() )){
-        //if($totalInCart > $x){
-        if($this->getTotalInCart($quote,$regex) > $x){
-            $discountData->setAmount($jplData->gettotalQty());
-        }
-    }
-}
 
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-//END TO MOVE TO JPLData applyDiscount()
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         return $discountData;
     }
 
